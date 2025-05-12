fc0a1afd492aa0f2bd551286299a1c9cbb4d456d
edson <edson.ferreira@alumni.usp.br>
2025-05-12 20:47:16 -0300

    Primeiro commit2

--- main.py
+++ main.py
@@ -173,8 +173,11 @@ def logout():
 #         db.create_all()
 #     app.run(debug=True)
     
-# Apenas executa em desenvolvimento
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# # Apenas executa em desenvolvimento
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
+
+# Replit precisa disso para expor o app
+app.run(host='0.0.0.0', port=81)
