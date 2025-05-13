85b4c418523896c20898afd34ca472e4a56a3745
edson <edson.ferreira@alumni.usp.br>
2025-05-12 21:37:07 -0300

    Primeiro commit2

--- main.py
+++ main.py
@@ -185,4 +185,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     port = int(os.environ.get("PORT", 81)) 
-    app.run(host='0.0.0.0', port=81)
+    app.run(host='0.0.0.0', port=port)
